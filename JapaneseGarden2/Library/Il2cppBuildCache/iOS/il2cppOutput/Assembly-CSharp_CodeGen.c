962464b41e472298187d809f1b0a35e6ca184003
faradraissa <100128390+faradraissa@users.noreply.github.com>
2022-03-22 17:23:36 +0000

    First Push

---
+++ JapaneseGarden2/Library/Il2cppBuildCache/iOS/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,99 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void PlacementIndicator::Start()
+extern void PlacementIndicator_Start_mC0F1E7B8F84514D2EDEA712785CDC9399A376200 (void);
+// 0x00000002 System.Void PlacementIndicator::Update()
+extern void PlacementIndicator_Update_m04B65B8903A034EA4C717E34BE6B8EF861382FE4 (void);
+// 0x00000003 System.Void PlacementIndicator::.ctor()
+extern void PlacementIndicator__ctor_mC5EF49C57C7F6519CA9E925EC1A0B13C7D3A08BE (void);
+// 0x00000004 System.Void ModelPlacer::Start()
+extern void ModelPlacer_Start_m6F0DA77C8EE4695AFECF9B3631090CF8C22B718C (void);
+// 0x00000005 System.Void ModelPlacer::Update()
+extern void ModelPlacer_Update_m143398C7438BAC62E106EC00458E707AD515AE89 (void);
+// 0x00000006 System.Void ModelPlacer::MoveSelected()
+extern void ModelPlacer_MoveSelected_m3A162F547667EF906A6C6C9880F28F9C8BA239EF (void);
+// 0x00000007 System.Void ModelPlacer::Select(UnityEngine.GameObject)
+extern void ModelPlacer_Select_mD13F08DEAD71412F41E5E66A9CA91BE5F56312F6 (void);
+// 0x00000008 System.Void ModelPlacer::Deselect()
+extern void ModelPlacer_Deselect_m2964BD25BDDF55F735C559C1331ADFA2229633A7 (void);
+// 0x00000009 System.Void ModelPlacer::ToggleSelectionVisual(UnityEngine.GameObject,System.Boolean)
+extern void ModelPlacer_ToggleSelectionVisual_mEB41218B7F52D19FCE29A44C42097C97F0971EB9 (void);
+// 0x0000000A System.Void ModelPlacer::PlaceModel(UnityEngine.GameObject)
+extern void ModelPlacer_PlaceModel_mABA8D5C554AC5388F42F53682C2286D95FA47AFB (void);
+// 0x0000000B System.Void ModelPlacer::ScaleSelected(System.Single)
+extern void ModelPlacer_ScaleSelected_mA71132B1C107B6230D8AFE9B2B740AF92FD496FF (void);
+// 0x0000000C System.Void ModelPlacer::RotateSelected(System.Single)
+extern void ModelPlacer_RotateSelected_mFEE63F1D562E22C9782FABEA5D31A99FB89DAD30 (void);
+// 0x0000000D System.Void ModelPlacer::DeleteSelected()
+extern void ModelPlacer_DeleteSelected_m77AD97DC6631324A5A402242E5148818C338977D (void);
+// 0x0000000E System.Void ModelPlacer::.ctor()
+extern void ModelPlacer__ctor_m9191FF81E3F63E5A50941B56E11495818BDAC758 (void);
+static Il2CppMethodPointer s_methodPointers[14] = 
+{
+	PlacementIndicator_Start_mC0F1E7B8F84514D2EDEA712785CDC9399A376200,
+	PlacementIndicator_Update_m04B65B8903A034EA4C717E34BE6B8EF861382FE4,
+	PlacementIndicator__ctor_mC5EF49C57C7F6519CA9E925EC1A0B13C7D3A08BE,
+	ModelPlacer_Start_m6F0DA77C8EE4695AFECF9B3631090CF8C22B718C,
+	ModelPlacer_Update_m143398C7438BAC62E106EC00458E707AD515AE89,
+	ModelPlacer_MoveSelected_m3A162F547667EF906A6C6C9880F28F9C8BA239EF,
+	ModelPlacer_Select_mD13F08DEAD71412F41E5E66A9CA91BE5F56312F6,
+	ModelPlacer_Deselect_m2964BD25BDDF55F735C559C1331ADFA2229633A7,
+	ModelPlacer_ToggleSelectionVisual_mEB41218B7F52D19FCE29A44C42097C97F0971EB9,
+	ModelPlacer_PlaceModel_mABA8D5C554AC5388F42F53682C2286D95FA47AFB,
+	ModelPlacer_ScaleSelected_mA71132B1C107B6230D8AFE9B2B740AF92FD496FF,
+	ModelPlacer_RotateSelected_mFEE63F1D562E22C9782FABEA5D31A99FB89DAD30,
+	ModelPlacer_DeleteSelected_m77AD97DC6631324A5A402242E5148818C338977D,
+	ModelPlacer__ctor_m9191FF81E3F63E5A50941B56E11495818BDAC758,
+};
+static const int32_t s_InvokerIndices[14] = 
+{
+	2441,
+	2441,
+	2441,
+	2441,
+	2441,
+	2441,
+	2001,
+	2441,
+	1108,
+	2001,
+	2022,
+	2022,
+	2441,
+	2441,
+};
+extern const CustomAttributesCacheGenerator g_AssemblyU2DCSharp_AttributeGenerators[];
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	14,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	g_AssemblyU2DCSharp_AttributeGenerators,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
